faffca661187b3876e8ae21ec9f436db980d31e3
Michael Waaler <michael.waaler@bouvet.no>
2025-02-11 14:04:17 +0100

    Updated gitignore

--- api/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ api/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77156f0a36825a5333ca71a771e741a7f17258bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4ee7f9f87e922610939d7f165abadd4ce0d4f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
